ed8622f4449f3691d5068b9c60b0f5d4aa3b3c6d
Michael Le <admin@michaelworkspace.com>
2020-12-07 18:54:01 -0500

    clean up work

--- day01.py
+++ day01.py
@@ -4,7 +4,7 @@ from typing import List
 def find_product(inputs: List[int]) -> int:
     """Given a list of integers, if the sum of two element is 2020, return it's product."""
 
-
+     # This is the classic Two Sum problem
      # This is not good solution because it is O(n^2)
 #    for x in INPUTS:
 #        for y in INPUTS:
@@ -14,7 +14,7 @@ def find_product(inputs: List[int]) -> int:
 
 
     # This is the optimal solution because its time complexity is O(n)
-    needs = [2020 - x for x in inputs]
+    needs = {2020 - x for x in inputs}
 
     for num in inputs:
         if num in needs:
@@ -29,7 +29,7 @@ def find_product_part2(inputs: List[int]) -> int:
 
 
     n = len(inputs)
-
+    # This is the classic Three Sum problem
     # Naive run time is O(n^3) cube which is not very efficient
     for i in range(n):
         for j in range(i+1, n):
